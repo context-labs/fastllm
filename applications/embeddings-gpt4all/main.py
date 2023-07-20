f6b63bcc7d1ba5f9bf9ed170ebb5da6b93db4ff8
Sam Heutmaker <samheutmaker@gmail.com>
2023-07-20 17:22:17 -0600

    playing with modal

--- applications/embeddings-gpt4all/main.py
+++ applications/embeddings-gpt4all/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from src import InputRequest, get_embedding, OpenAIEmbeddingOutput
+from modal import Image, Stub, asgi_app
 
 app = FastAPI()
+stub = Stub()
 
 
 @app.post("/v1/embeddings", response_model=OpenAIEmbeddingOutput)
 def process_embedding(data: InputRequest):
     return get_embedding(data)
+
+
+@asgi_app()
+def fastapi_app():
+    return app
